7fb469b2f12a2a36f555b7d8bcf5e920bc7ece67
Matt <mchan00@stuy.edu>
2019-10-08 09:17:43 -0400

    Completed work

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -1,4 +1,4 @@
-# Team "TBD"
+# Team "τεαμ"
 # Matthew Chan & Ethan Chen
 # SoftDev1 pd2
 # K#17: No Trouble
@@ -15,17 +15,19 @@ c = db.cursor()               #facilitate db ops
 
 #==========================================================
 
-
+c.execute("CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER);")
 
 with open('courses.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
+    for row in reader:
+        c.execute("INSERT INTO courses VALUES('%s', %d, %d);" % (row['code'], int(row['mark']), int(row['id'])))
+
+c.execute("CREATE TABLE students(name TEXT, age INTEGER, id INTEGER);")
 
 with open('students.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
-
-
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+    for row in reader:
+        c.execute("INSERT INTO students VALUES('%s', %d, %d);" % (row['name'], int(row['age']), int(row['id'])))
 
 #==========================================================
 
