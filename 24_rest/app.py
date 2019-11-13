4d087f37bd90e74b7f165aa51640ea3a190c339f
Matt <mchan00@stuy.edu>
2019-11-12 22:12:10 -0500

    Finished work

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -5,18 +5,19 @@
 
 from flask import Flask
 from flask import render_template
-from urllib3 import urlopen
+from urllib.request import urlopen
 import json
 
 app = Flask(__name__)   #create instance of class Flask
 
 @app.route("/")
 def root():
-    u = urllib3.urlopen("https://api.nasa.gov/planetary/apod?api_key=kPinVCLcM17ri49dnZA0pd0pGlnfZhh7g7OyR0Fo")
+    u = urlopen("https://api.nasa.gov/planetary/apod?api_key=kPinVCLcM17ri49dnZA0pd0pGlnfZhh7g7OyR0Fo")
     response = u.read()
     data = json.loads(response)
     return render_template("index.html",
-                            pic = data['url'])
+                            pic = data['url'],
+                            caption = data['explanation'])
 
 if __name__ == "__main__":
     app.debug = True
