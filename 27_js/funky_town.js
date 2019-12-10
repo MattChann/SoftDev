4b9a15756b932d68902439392fa3e7d4ca17bb0e
Matt <mchan00@stuy.edu>
2019-12-10 09:32:13 -0500

    Gcd work

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -7,5 +7,16 @@ var fibonacci = function(n) {
             return 1;
         }
     }
-    return fibonacci(n-2) + fibonacci(n-1)
+    return fibonacci(n-2) + fibonacci(n-1);
+}
+
+var gcd = (a, b) => {
+    let greatest = 1;
+    let i;
+    for(i=1; i<=Math.min(a,b); i++) {
+        if ((a%i == 0) && (b%i == 0)) {
+            greatest = i;
+        }
+    }
+    return greatest;
 }
