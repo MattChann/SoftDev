426671cf8969995b63514f2c5b5662c36872bc45
Matt <mchan00@stuy.edu>
2019-12-10 09:26:17 -0500

    Fibonacci work

---
+++ 27_js/funky_town.js
@@ -0,0 +1,11 @@
+var fibonacci = function(n) {
+    if (n == 0) {
+        return 0;
+    }
+    else {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+    }
+    return fibonacci(n-2) + fibonacci(n-1)
+}
