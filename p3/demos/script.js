41e02350c83d715a882ed2ae1b2c35c9d182f2a6
Matthew Chan <mchan00@stuy.edu>
2020-03-31 12:49:55 -0400

    Content finished

--- p3/demos/script.js
+++ p3/demos/script.js
@@ -1,1 +1,5 @@
-gsap.to(".box", {duration: 1, x: "-100", y: 300, scale: 0.5});
+var tl = gsap.timeline();
+
+tl.from(".box", {duration: 1, x: "-100", y: 300, scale: 0.5});
+tl.to(".box", {duration: 1, x: 300});
+tl.to(".box", {duration: 1, y: 500, opacity: "50%", scale: 3});
